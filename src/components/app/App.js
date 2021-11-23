1c356f1fcfbbf078ecad91437cf484003e5a2525
Dmitry Podkyiko <dimkapodkyiko@gmail.com>
2021-11-23 17:44:10 +0300

    реализовал кнопки выбора мода таймера, !!!сделать кнопку старт и прикрутить таймер

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,38 @@
+import {Component} from 'react'
 import Head from "../head/head"
 import Timer from "../timer/timer"
 
 import './app.css';
 
-function App() {
-  return (
-    <div className="container">
-      <Head/>
-      <div className="body">
-        <Timer/>
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      settings:{
+        time:25,
+        activeBtnMode:'pomodoro',
+        activeBtnStart:false
+      }
+    }
+  }
+  onChangeActiveBtnMode=(mode)=>{
+    debugger
+    this.setState(state=>{
+      state.settings.activeBtnMode = mode
+      return {state}
+    })
+  }
+  render(){
+    return (
+      <div className="container">
+        <Head/>
+        <div className="body">
+          <Timer onChangeActiveBtnMode ={this.onChangeActiveBtnMode} settings={this.state.settings}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -2,16 +2,38 @@
 import './timer.css'
 
 const Timer =(props)=>{
+    const {time,activeBtnMode,activeBtnStart} = props.settings
+
+    const buttonData  =[
+        {name:'pomodoro', label:'Pomodoro'},
+        {name:'shortBreak', label:'Short Break'},
+        {name:'longBreak', label:'Long Break'}
+    ]
+    const onChangeActiveBtnMode=(e)=>{
+        props.onChangeActiveBtnMode(e.target.name)
+    }
+    const buttons = buttonData.map(item =>{
+        const active = activeBtnMode === item.name;
+        const clazz = active ? "timer-btn__mode-active" :  "timer-btn__mode";
+        return <button 
+                onClick={onChangeActiveBtnMode}
+                className={clazz}
+                name={item.name}
+                key = {item.name}
+                type="button">
+                    {item.label}
+            </button>
+    })
+    const clazzStart = activeBtnStart ? "timer-btn-stop" : "timer-btn-start";
+    const valueBtnStart= activeBtnStart ? "stop" : "start";
     return(
         <div className="timer">
             <div className="timer-btn">
-                
+                {buttons}
             </div>
-            <div className="timer-string">
-                    
-            </div>
-            <div className="timer-btn-start">
-                    
+            <div className="timer-string">{time}</div>
+            <div className="">
+                <button className={clazzStart}>{valueBtnStart}</button>
             </div>
         </div>
     )
